860fda51e24fa6e7874eac0c2f9e6cea5285c78b
commGom <hyunwoo921129@gmail.com>
2021-12-17 01:14:23 +0900

    홈화면, 회원 가입, 회원 목록 보기 구현

--- src/main/java/commGom/hellospring/SpringConfig.java
+++ src/main/java/commGom/hellospring/SpringConfig.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Configuration;
 //java를 이용한 직접 bean 등록, 유용하다.
 @Configuration
 public class SpringConfig {
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
 }
