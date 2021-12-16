1a13ffc6c178b2feb06a9dab3774ab0481f04051
commGom <hyunwoo921129@gmail.com>
2021-12-17 00:46:07 +0900

    basic spring, spring bean, dependency injection, Junit test

--- src/main/java/commGom/hellospring/SpringConfig.java
+++ src/main/java/commGom/hellospring/SpringConfig.java
@@ -5,7 +5,7 @@ import commGom.hellospring.repository.MemoryMemberRepository;
 import commGom.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//java를 이용한 직접 bean 등록, 유용하다.
 @Configuration
 public class SpringConfig {
     @Bean
